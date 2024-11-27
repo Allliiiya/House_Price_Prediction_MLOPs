a683ed3725b48cc5e1aec4614f6601cdebf944dd
ChloeA27 <blessanq@gmail.com>
2024-11-26 22:20:51 -0500

    create the elastic net regressor model to compare

--- Airflow/dags/modeling_and_eval_dag.py
+++ Airflow/dags/modeling_and_eval_dag.py
@@ -11,6 +11,7 @@ from src.bias_detection import detect_model_bias, evaluate_bias_disparity
 from src.model_rf import train_and_predict_rf
 from src.modeling_and_evaluation import train_and_predict
 from src.model_rf import evaluate_model
+from src.model_elastic_net import train_and_predict_elastic_net
 
 # Define default arguments for your DAG
 default_args = {
@@ -30,7 +31,7 @@ dag3 = DAG(
     max_active_runs=1,
 )
 
-# Task to train the model and make predictions
+# Task to train the linear regression model and make predictions
 def train_and_predict_callable(**kwargs):
     ti = kwargs['ti']
 
@@ -74,7 +75,7 @@ train_and_predict_task = PythonOperator(
     dag=dag3,
 )
 
-
+# Task to train the random forest regression model
 def train_and_predict_rf_callable(**kwargs):
     ti = kwargs['ti']
 
@@ -102,6 +103,36 @@ train_and_predict_rf_task = PythonOperator(
     dag=dag3,
 )
 
+# Task to train the Elastic Net regression model
+def train_and_predict_elastic_net_callable(**kwargs):
+    ti = kwargs['ti']
+
+    # Access the configuration passed from dag1
+    conf = kwargs.get("dag_run").conf
+    augmented_data = conf.get('augmented_data')
+    test_data = conf.get('test_data')
+    combined_features = conf.get('combined_features')
+
+    #alpha = conf.get('alpha', 0.1) 
+    #l1_ratio = conf.get('l1_ratio', 0.5)  # Default value if not provided
+
+    augmented_data = pd.read_json(augmented_data, orient='split')
+    test_data = pd.read_json(test_data, orient='split')
+
+    # Train and predict with Elastic Net Regressor
+    y_pred, model = train_and_predict_elastic_net(augmented_data, test_data, combined_features)
+
+    # Push predictions and model to XCom
+    ti.xcom_push(key='elastic_net_y_pred', value=y_pred.tolist())
+    # Note: Model objects are not pushed directly to XCom to avoid serialization issues.
+
+train_and_predict_elastic_net_task = PythonOperator(
+    task_id='train_and_predict_elastic_task',
+    python_callable=train_and_predict_elastic_net_callable,
+    provide_context=True,
+    dag=dag3,
+)
+
 # Task to evaluate the model's performance
 def evaluate_and_compare_models_callable(**kwargs):
     ti = kwargs['ti']
@@ -110,6 +141,7 @@ def evaluate_and_compare_models_callable(**kwargs):
      # Pull predictions for both models
     linear_y_pred = ti.xcom_pull(task_ids='train_and_predict_task', key='y_pred')  # Linear regression
     rf_y_pred = ti.xcom_pull(task_ids='train_and_predict_rf_task', key='rf_y_pred')  # Random Forest
+    elastic_y_pred = ti.xcom_pull(task_ids='train_and_predict_elastic_task', key='elastic_net_y_pred')  # Elastic net
 
 
     # Access the configuration
@@ -144,6 +176,12 @@ def evaluate_and_compare_models_callable(**kwargs):
         rf_metrics = evaluate_model(test_data, np.array(rf_y_pred))
         rf_metrics["Model"] = "Random Forest Regressor"
         metrics.append(rf_metrics)
+
+        # Evaluate elastic net Regressor
+    if elastic_y_pred:
+        elastic_net_metrics = evaluate_model(test_data, np.array(elastic_y_pred))
+        elastic_net_metrics["Model"] = "Elastic Net Regressor"
+        metrics.append(elastic_net_metrics)    
 
     # # Create comparison DataFrame
     # comparison_df = pd.DataFrame({
@@ -184,6 +222,7 @@ def detect_model_bias_callable(**kwargs):
     # Pull predictions from XCom
     linear_y_pred = ti.xcom_pull(task_ids='train_and_predict_task', key='y_pred')  # Linear Regression
     rf_y_pred = ti.xcom_pull(task_ids='train_and_predict_rf_task', key='rf_y_pred')  # Random Forest
+    elastic_y_pred = ti.xcom_pull(task_ids='train_and_predict_elastic_task', key='elastic_net_y_pred')  # Random Forest
 
 
     if test_data_json is None or (linear_y_pred is None and rf_y_pred is None):
@@ -216,6 +255,12 @@ def detect_model_bias_callable(**kwargs):
         rf_y_pred = np.array(rf_y_pred)
         bias_metrics_rf = detect_model_bias(test_data, rf_y_pred, encoded_features, target)
         bias_metrics_all['Random Forest'] = bias_metrics_rf
+
+    # Detect bias for Elastic Net Regressor
+    if elastic_y_pred:
+        elastic_y_pred = np.array(elastic_y_pred)
+        bias_metrics_elastic_net = detect_model_bias(test_data, elastic_y_pred, encoded_features, target)
+        bias_metrics_all['Elastic Net'] = bias_metrics_elastic_net
 
     # logging.info(f"Bias metrics:\n{bias_metrics}")
     # # Push bias metrics to XCom
@@ -293,4 +338,4 @@ dag=dag3,)
 
 
 # Set dependencies for tasks within dag3
-[train_and_predict_task, train_and_predict_rf_task]>> evaluate_and_compare_task >> bias_detection_task >> bias_disparity_evaluation_task
+[train_and_predict_task, train_and_predict_rf_task, train_and_predict_elastic_net_task]>> evaluate_and_compare_task >> bias_detection_task >> bias_disparity_evaluation_task
---
+++ Airflow/dags/src/model_elastic_net.py
@@ -0,0 +1,73 @@
+from sklearn.linear_model import ElasticNet
+from sklearn.metrics import mean_squared_error
+import mlflow
+import mlflow.sklearn
+import numpy as np
+import pandas as pd
+from src.modeling_and_evaluation import evaluate_model
+
+# Function to train the model and make predictions
+def train_and_predict_elastic_net(augmented_data, test_data, combined_features):
+    """
+    Trains an elastic net Regressor model and predicts on the test set.
+
+    Parameters:
+    - augmented_data (pd.DataFrame): Training dataset with features and target.
+    - test_data (pd.DataFrame): Test dataset with features.
+    - combined_features (list): List of feature columns.
+
+    Returns:
+    - np.ndarray: Predicted values for the test set.
+    """
+    X_train = augmented_data[combined_features]
+    y_train = augmented_data['SalePrice']
+    X_test = test_data[combined_features]
+    y_test = test_data['SalePrice']
+
+    # Set the experiment    
+    mlflow.set_experiment("Elastic Net Regressor Experiment")
+
+   # Start MLflow run
+    with mlflow.start_run():
+        model = ElasticNet(alpha=1.0, l1_ratio=0.5, random_state=42) # l1=0.5 is the balance of ridge and lasso
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        
+        # Calculate RMSE
+        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        
+        # Log metrics, parameters, and model
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_param("model_type", "Elastic Net Regressor")
+        mlflow.log_param("alpha", 1.0)
+        mlflow.log_param("l1_ratio", 0.5)
+        
+        # Log model
+        mlflow.sklearn.log_model(model, "model")
+        return y_pred, model
+
+def compare_models_elastic_net(train_data, augmented_data, test_data, combined_features):
+    """
+    Compares model performance using original and augmented training data.
+
+    Parameters:
+    - train_data (pd.DataFrame): Original training dataset with 'SalePrice' target.
+    - augmented_data (pd.DataFrame): Augmented training dataset with 'SalePrice' target.
+    - test_data (pd.DataFrame): Test dataset with 'SalePrice' target.
+    - combined_features (list): List of feature columns.
+
+    Returns:
+    - pd.DataFrame: Comparison of evaluation metrics for original and augmented data.
+    """
+    # Train and evaluate on original training data
+    y_pred_original, _ = train_and_predict_elastic_net(train_data, test_data, combined_features)
+    original_metrics = evaluate_model(test_data, y_pred_original)
+
+    # Train and evaluate on augmented training data
+    y_pred_augmented, _ = train_and_predict_elastic_net(augmented_data, test_data, combined_features)
+    augmented_metrics = evaluate_model(test_data, y_pred_augmented)
+
+    # Compare the metrics in a DataFrame
+    comparison_df = pd.DataFrame([original_metrics, augmented_metrics], index=["Original Data", "Augmented Data"])
+
+    return comparison_df
